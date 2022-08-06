067c6991a6620e9e1914f865625e92f23194985f
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-06 01:49:52 +0100

    Added test folders

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module is designed to test square.py
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Class to define the unittest
+    """
+
+    def test_base(self):
+        """
+        Test the BaseModel class
+        """
